34a2d086cae9dd0d4e2e28a7801ad782c5164cdf
xancelly <xancelly@yandex.ru>
2022-10-21 21:24:33 +0300

    MainActivity.kt and fragments was added

---
+++ app/src/main/java/com/example/avitotechweather/presentation/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.avitotechweather.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.avitotechweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
